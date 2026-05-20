de58a238a0596fa6b071eb587bb24361e727ae0a
Josh <josh@claw-code.dev>
2026-05-20 08:35:04 -0500

    buildApp.sh: update ANDROID_HOME path to /home/josh/android-sdk

--- buildApp.sh
+++ buildApp.sh
@@ -6,7 +6,7 @@ set -e
 
 cd /home/josh/claw-code/DeFide
 
-export ANDROID_HOME="/home/josh/claw-code/DeFide/android-sdk"
+export ANDROID_HOME="/home/josh/android-sdk"
 export JAVA_HOME="/usr/lib/jvm/java-21-openjdk-amd64"
 export GRADLE_USER_HOME="/home/josh/claw-code/DeFide/.gradle-home"
 
