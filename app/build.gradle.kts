a84de3d78b337b194e85ca632d78b2a0a9a163d3
Tristin Baker <tristinbaker52@gmail.com>
2026-07-08 07:38:04 -0400

    Bump to v2.0.8 (versionCode 25)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.tristinbaker.defide"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "2.0.7"
+        versionCode = 25
+        versionName = "2.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
